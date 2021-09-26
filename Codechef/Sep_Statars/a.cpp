cdc6238f55ab6a6da9e7a59b39bd39b77691c1de
Nirav <gusainirav1@gmail.com>
2021-09-26 16:20:01 +0530

    grinding

---
+++ Codechef/Sep_Statars/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve(){
+    
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll q;
+    cin >> q;
+    while (q--)
+    {
+        solve();
+    }
+    return 0;
+}
